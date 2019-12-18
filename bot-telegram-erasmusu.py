9b31a9be31074399d278c5e81514083582bec138
Efren-cmd <58843077+Efren-cmd@users.noreply.github.com>
2019-12-18 22:43:39 +0100

    Update bot-telegram-erasmusu.py

--- bot-telegram-erasmusu.py
+++ bot-telegram-erasmusu.py
@@ -114,7 +114,7 @@ def main():
 
     # Add conversation handler with predefined states:
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('start', start),CommandHandler('menu', menu)],
 
         states={
             SET_LANG: [MessageHandler(Filters.regex('^(ES|EN)$'), set_lang)],
