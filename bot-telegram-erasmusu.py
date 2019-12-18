bfb241f9e5fe6a1aa1f3c3b37766a18337ea471d
Efren-cmd <58843077+Efren-cmd@users.noreply.github.com>
2019-12-18 14:11:31 +0100

    Update bot-telegram-erasmusu.py

--- bot-telegram-erasmusu.py
+++ bot-telegram-erasmusu.py
@@ -3,6 +3,7 @@
 Basic example for a bot that uses inline keyboards.
 """
 import logging
+import os
 import telegram.ext
 from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler
 from telegram.ext import ConversationHandler, CallbackQueryHandler, Filters
