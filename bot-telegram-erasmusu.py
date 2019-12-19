eaa71a2b149a6bd76012a7c058f8a708a7f55f5d
Efren <efren@erasmusu.com>
2019-12-19 16:31:24 +0100

    Cambios

--- bot-telegram-erasmusu.py
+++ bot-telegram-erasmusu.py
@@ -33,7 +33,7 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
     "Hi, please select a language to start. / Hola, por favor selecciona un idioma para comenzar.",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup=ReplyKeyboardMarkup(reply_markup=reply_markup))
 
     return SET_LANG
 
@@ -46,7 +46,7 @@ def menu(update, context):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
     "Hi, please select a language to start. / Hola, por favor selecciona un idioma para comenzar.",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup=ReplyKeyboardMarkup(reply_markup=reply_markup))
 
     return SET_LANG
 
