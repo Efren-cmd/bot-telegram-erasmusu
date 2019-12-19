76598f9f4c35735c7a06fab70fedb4be40c42fed
Efren <efren@erasmusu.com>
2019-12-19 17:15:06 +0100

    cambios

--- bot-telegram-erasmusu.py
+++ bot-telegram-erasmusu.py
@@ -26,11 +26,12 @@ TOKEN = os.environ['TOKEN']
 #Start def
 
 def start(update, context):
+    
     reply_keyboard = [[InlineKeyboardButton('EN', callback_data='EN'), InlineKeyboardButton('ES',callback_data='ES')]]
     reply_markup = InlineKeyboardMarkup(reply_keyboard)
     update.message.reply_text(
     "Hi, please select a language to start. / Hola, por favor selecciona un idioma para comenzar.",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup=reply_markup)
 
     return SET_LANG
 
@@ -40,7 +41,7 @@ def menu(update, context):
     reply_markup = InlineKeyboardMarkup(reply_keyboard)
     update.message.reply_text(
     "Hi, please select a language to start. / Hola, por favor selecciona un idioma para comenzar.",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        reply_markup=reply_markup)
 
     return SET_LANG
 
